4c1d0a94ea185512b81c5df546886c3371f56335
st3benm <stevenmontalvan@gmail.com>
2025-11-11 09:37:25 -0500

    modicficacion

--- taller.js
+++ taller.js
@@ -6,81 +6,111 @@ class tallerDos {
     }
     serieFibonacci() {
         let a = 0, b = 1, c;
-        console.log(a);
-        console.log(b);
+        const numero = this.numeros
+        let lista = [];
+        if (numero <= 0) {
+            console.log("El número debe ser mayor a 0");
+            return [];
+        }
+
+        lista.push(a)
+        if (numero > 1) lista.push(b)
         for (let i = 3; i <= this.numeros; i++) {
             c = a + b
-            console.log(c)
+            lista.push(c)
             a = b;
             b = c;
 
         }
+        console.log(`Serie de Fibonacci (${numero}):`);
+        for (let i = 0; i < lista.length; i++) {
+            console.log(lista[i]);
+        }
+        return lista;
 
     }
     divisores() {
         let cv = 0;
+        let lista = [];
         const numero = this.numeros
         console.log(`Divisores de ${numero}`)
         for (let c = 1; c <= numero; c++) {
             if (numero % c === 0) {
+                lista.push(c);
                 console.log(c)
                 cv++
             }
         }
         console.log(`Numeros divisores de ${numero}: ${cv}`)
+        return lista;
 
 
     }
     sumaDivisores() {
         const numero = this.numeros
         let suma = 0;
+        let lista = [];
         for (let i = 1; i <= numero; i++) {
             if (numero % i === 0) {
+                lista.push(i)
                 suma += i
             }
         }
         console.log(`Suma de divisores es:${suma}`)
+        return suma
     }
     numeroPerfecto() {
         let suma = 0
         const numero = this.numeros
+        let lista = [];
         for (let c = 1; c < numero; c++) {
             if (numero % c === 0) {
+                lista.push(c)
                 suma += c
             }
         }
         if (suma === numero) {
             console.log(`El numero ${numero} es perfecto`)
+            return true;
         } else {
             console.log(`Su numero "${numero} no es perfecto `)
+            return false;
         }
     }
 
     numeroPrimo() {
         const numero = this.numeros
-        let cv = 0;
+        let lista = [];
         for (let c = 2; c < numero; c++) {
             if (numero % c === 0) {
-                cv++
+                lista.push(c);
+
+
 
             }
         }
-        if (cv === 0) {
+        if (lista.length === 0) {
             console.log(`Su numero es primo`)
+            return true;
         } else {
             console.log(`Su numero no es primo`)
+            console.log(`Sus numeros divisibles son ${lista}`)
+            return false;
         }
     }
     invertirNumero() {
         let numero = this.numeros
         let invertido = 0;
+        let lista = [];
         while (numero > 0) {
             let digito = numero % 10;
+            lista.push(digito)
             invertido = invertido * 10 + digito
             numero = Math.floor(numero / 10)
 
         }
         console.log(`Numero invertido es ${invertido}`)
+        return invertido;
     }
     contarDigitos() {
         let numero = this.numeros
@@ -94,37 +124,49 @@ class tallerDos {
             }
         }
         console.log(`El numero tiene ${cv} digitos `)
+        return cv;
     }
     factoriales() {
         let numero = this.numeros
         let fact = 1;
+        let lista = [];
         for (let c = 1; c <= numero; c++) {
             fact *= c;
+            lista.push(c)
 
         }
         console.log(`El numero factorial de ${numero} es:${fact}`)
+        return fact
     }
     multiplicarVeces() {
         const veces = this.veces
         let numero = this.numeros
         let suma = 0;
+        let lista = [];
         for (let c = 1; c <= veces; c++) {
             suma += numero
+            lista.push(suma)
 
         }
         console.log(`La respues de la suma es ${suma}`)
+        return suma
     }
     restar() {
         const numero2 = this.numeros2
         let numero = this.numeros
+        let lista = [];
+        if (numero2 <= 0) {
+            console.log("El numero a restar (numero2) debe ser mayor que 0");
+            return null;
+        }
         while (numero > 0) {
             numero = numero - numero2
+            lista.push(numero)
 
         }
         console.log(`Su resta es: ${numero}`)
+        return numero
     }
-
-
 }
 
 let ejer1 = new tallerDos({ numero: 8 })
@@ -148,3 +190,21 @@ ejer9.multiplicarVeces();
 let ejer10 = new tallerDos({ numero: 5, numero2: 4 })
 ejer10.restar();
 
+function paresFibonacci() {
+    const numeros = [{ numero: 5 }, { numero: 8 }, { numero: 10 }]
+    for (let c = 0; c < numeros.length; c++) {
+        let Fibonacci = new tallerDos(numeros[c])
+        Fibonacci.serieFibonacci()
+        console.log(`------------------`)
+    }
+}
+function variosDivisores() {
+    const numeros = [{ numero: 6 }, { numero: 12 }, { numero: 28 }]
+    for (let c = 0; c < numeros.length; c++) {
+        let divisores = new tallerDos(numeros[c])
+        divisores.divisores()
+
+
+    }
+
+}
